6010c2e48dc45a251494fdb5607498f7000092af
Belal-2000 <belalma3arek#@gmail.com>
2022-04-28 19:18:39 +0200

    Added image class that can be used all over the site

--- script/js.js
+++ script/js.js
@@ -1,2 +1,19 @@
+// set img src
+const IMAGES = document.querySelectorAll(".card-img img");
+IMAGES.forEach((e, i) => {
+    e.setAttribute("src", `../images/project-${i + 1}.jpg`);
+});
 
+const FILTERBUTTONS = document.querySelectorAll(".filter-Buttons button");
 
+FILTERBUTTONS.forEach((button) => {
+    button.addEventListener("click", () => {
+        // remove active class from all buttons
+        FILTERBUTTONS.forEach((button) => {
+            button.classList.remove("active");
+        });
+
+        // add active class to target button
+        button.classList.add("active");
+    });
+});
